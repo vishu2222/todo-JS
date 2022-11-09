61ce68f0d35c1db7ad61a269103ce2967fffe2e1
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-09 11:49:16 +0530

    removed comments

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ button.addEventListener('click', (event) => {
   const text = document.getElementById('inputTxt') // input todo text
   todosArr.push({ id: count, txt: text.value })
   count++
-  // todosArr.push(text.value)
   text.value = '' // clearing input entry or a todo
   displayTodos()
   event.preventDefault() // disable default actions if the event is not explicitely handled. // Clicking on a "Submit" button, prevent it from submitting a form
@@ -31,14 +30,3 @@ function displayTodos () {
   todoDiv.appendChild(containerDiv)
 }
 
-// function displayTodos () {
-//   const ul = document.createElement('ul')
-//   todosArr.forEach(item => {
-//     const li = document.createElement('div')
-//     li.textContent = item
-//     ul.appendChild(li)
-//   })
-//   const divTodos = document.querySelector('.todos') // div todo's
-//   divTodos.removeChild(divTodos.firstChild)
-//   divTodos.appendChild(ul)
-// }
