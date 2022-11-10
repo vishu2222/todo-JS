c62cbaf9b6f1825a0e2818a3278e146256ffa1af
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-10 21:52:09 +0530

    added event listeners buttons date check box etc

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 let todosArr = []
 let count = 0
- 
+
 const button = document.getElementById('submitTodo')
 button.addEventListener('click', (event) => {
   const text = document.getElementById('inputTxt') // input todo text
-  if (text.value.length === 0) displayTodos()
-  else {
+  if (text.value.length !== 0) {
     todosArr.push({ id: count, txt: text.value })
     count++
     text.value = '' // clearing input entry after a submit
@@ -26,10 +25,10 @@ function makeItemDiv (todo) {
   itemDiv.id = todo.id
   itemDiv.className = 'todoItem'
 
-  const checkBox = addCheckBox()
+  const checkBox = addCheckBox(todo)
   const textInput = addTextInput(todo)
-  const notes = addNotes()
-  const date = addDate()
+  const notes = addNotes(todo)
+  const date = addDate(todo)
   const priority = addPriority(todo)
   const delButton = addDelButton(todo)
 
@@ -43,9 +42,13 @@ function makeItemDiv (todo) {
   return itemDiv
 }
 
-function addCheckBox () {
+function addCheckBox (todo) {
   const checkBox = document.createElement('input')
-  checkBox.type = 'checkbox' // checkBox.setAttribute('type', 'checkbox')
+  checkBox.type = 'checkbox' // checkBox.setAttribute('type', 'checkbox') 
+  todo.checkBox = checkBox.checked
+  checkBox.addEventListener('change', () => {
+    todo.checkBox = checkBox.checked
+  })
   return checkBox
 }
 
@@ -53,43 +56,69 @@ function addTextInput (todo) {
   const itemInput = document.createElement('input')
   itemInput.type = 'text'
   itemInput.value = todo.txt
+  itemInput.addEventListener('change', () => {
+    todo.txt = itemInput.value
+  })
   return itemInput
 }
 
-function addNotes () {
+function addNotes (todo) {
   const notes = document.createElement('input')
   notes.type = 'textarea'
   notes.placeholder = 'Notes'
+  todo.notes = notes.value
+  notes.addEventListener('change', () => {
+    todo.notes = notes.value
+  })
   return notes
 }
 
-function addDate () {
+function addDate (todo) {
   const date = document.createElement('input')
   date.type = 'date' // date.valueAsDate = new Date()
+  todo.date = date.value
+  date.addEventListener('change', () => {
+    todo.date = date.value
+  })
   return date
 }
 
 function addPriority (todo) {
-  const priority = document.createElement('select')
-  priority.id = 'priority' + String(todo.id)
-  priority.class = 'priority'
+  const priority = document.createElement('select') // priority.id = 'priority' + String(todo.id) // priority.class = 'priority'
+  todo.priority = priority.value
   const priorityOptions = ['Low', 'Medium', 'High']
-
   priorityOptions.forEach(p => {
     const option = document.createElement('option')
     option.textContent = p
     priority.appendChild(option)
+  })
+  priority.addEventListener('change', () => {
+    todo.priority = priority.value
   })
   return priority
 }
 
 function addDelButton (todo) {
   const delButton = document.createElement('button') // delButton.className = 'delTodo' // delButton.id = 'delTodo' + String(todo.id)
-  delButton.textContent = 'Delete' // delButton.setAttribute('textContent', 'Delete') didnt work
+  delButton.textContent = 'Delete' // delButton.setAttribute('textContent', 'Delete') didnt work?
   const id = todo.id
+
   delButton.addEventListener('click', () => {
     document.getElementById(id).remove()
     todosArr = todosArr.filter(item => item.id !== id)
   })
   return delButton
 }
+
+// function addUpdateButton (todo) {
+//   const updateButton = document.createElement('button')
+//   updateButton.textContent = 'Update'
+//   const id = todo.id
+
+//   updateButton.addEventListener('click', () => {
+//     // todo.checkBox =
+//   })
+
+//   return updateButton
+// }
+
