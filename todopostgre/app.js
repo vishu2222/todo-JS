b1e37f4e21e4d0dce3d3fc8c8394013f72475164
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-16 14:09:30 +0530

    added pg Client

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const { Client } = require('pg')
 // const path = require('path')
 
+const clientDb = new Client({
+  user: 'todouser',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'todo',
+  port: 5432
+})
+
+clientDb.connect((err) => {
+  if (err) { throw err }
+  console.log('connected')
+})
+console.log(clientDb)
 const app = express()
 app.use(express.static('public'))
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'))
-// })
 
 app.listen(3000)
