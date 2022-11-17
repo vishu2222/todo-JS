38d5464f1bf340c5460858c52a1a0b3d20dcb755
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-17 21:58:51 +0530

    changed cjs to es  imports, sql to queries.js

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -1,29 +1,22 @@
-const express = require('express')
-const { Client } = require('pg')
+import  express  from "express"
+import { connectDb, getTodos } from './db/queries.js'
 // const path = require('path')
 
 const app = express()
 app.use(express.static('static')) // middleware express function to serve static files in a directory
-
-const client = new Client({
-  user: 'todouser',
-  host: 'localhost',
-  database: 'tododb',
-  password: 'todo',
-  port: 5432
-})
-
-client.connect((err) => {
-  if (err) console.log(err)
-  else console.log('connected')
-})
+connectDb()
 
 app.get('/todos', async (req, res) => {
-  const todos = await (client.query('select * from todoSchema.todotable'))
-  console.log(todos.rows)
+  const todos = await getTodos()
   res.send(todos.rows)
-  // res.json(todos.rows) // returns a promise which resolves with the result of parsing the body text as JSON. it then internally calls res.send to send data. both res.send and res.json send json formated data, the diff is that res.json also converts invalid json types to json
 })
+
+// app.get('/todos', async (req, res) => {
+//   const todos = await (client.query('select * from todoSchema.todotable'))
+//   console.log(todos.rows)
+//   res.send(todos.rows)
+//   // res.json(todos.rows) // returns a promise which resolves with the result of parsing the body text as JSON. it then internally calls res.send to send data. both res.send and res.json send json formated data, the diff is that res.json also converts invalid json types to json
+// })
 
 app.listen(3000)
 
