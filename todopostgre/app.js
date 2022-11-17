96d2503978c0d871ff3b2c68f973f0f6328e2203
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-17 12:58:29 +0530

    added route /todos

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -2,20 +2,28 @@ const express = require('express')
 const { Client } = require('pg')
 // const path = require('path')
 
-const pgClient = new Client({
+const app = express()
+app.use(express.static('public'))
+
+const client = new Client({
   user: 'todouser',
   host: 'localhost',
-  database: 'postgres',
+  database: 'tododb',
   password: 'todo',
   port: 5432
 })
 
-pgClient.connect((err) => {
-  if (err) { throw err }
-  console.log('connected')
+client.connect((err) => {
+  if (err) console.log(err)
+  else console.log('connected')
 })
-console.log(pgClient)
-const app = express()
-app.use(express.static('public'))
+
+app.get('/todos', async (req, res) => {
+  const todos = await (client.query('select * from todoSchema.todotable'))
+  res.json(todos.rows)
+})
 
 app.listen(3000)
+
+// client.query(`INSERT INTO todoschema.todotable (id) VALUES (1);`)
+//   .then(res => console.log(res.rows))
