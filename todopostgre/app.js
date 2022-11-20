28495ca6a0be11831ddc3944ac7a3da7051fa80b
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-20 20:41:44 +0530

    res.send to res.json

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -1,24 +1,32 @@
 import  express  from "express"
-import { connectDb, getTodos } from './db/queries.js'
+import { connectDb, getTodos, insertTodo } from './db/queries.js'
 // const path = require('path')
 
 const app = express()
-app.use(express.static('static')) // middleware express function to serve static files in a directory
+
+// app.use((req, res, next) => {
+//   console.log(req.path)
+//   next()
+// })
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded  // app.use(express.json()) // for parsing application/json // https://expressjs.com/en/4x/api.html#req
+
 connectDb()
 
 app.get('/todos', async (req, res) => {
   const todos = await getTodos()
-  res.send(todos.rows)
+  res.json(todos) // *
 })
 
-// app.get('/todos', async (req, res) => {
-//   const todos = await (client.query('select * from todoSchema.todotable'))
-//   console.log(todos.rows)
-//   res.send(todos.rows)
-//   // res.json(todos.rows) // returns a promise which resolves with the result of parsing the body text as JSON. it then internally calls res.send to send data. both res.send and res.json send json formated data, the diff is that res.json also converts invalid json types to json
-// })
+app.post('/addTodo', async (req, res) => {
+  console.log(req)
+  // await insertTodo(req.body)
+  // res.send('200')
+  res.json('todo added')
+  // res.redirect('/')
+})
+
+app.use(express.static('static')) // middleware express function to serve static files in a directory
+
 
 app.listen(3000)
-
-// client.query(`INSERT INTO todoschema.todotable (id) VALUES (1);`)
-//   .then(res => console.log(res.rows))
