98ac4e30cd01ae96d8283b94bc4e7ce15fd8e765
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-21 10:53:52 +0530

    added bodyParser and post-request

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -1,32 +1,36 @@
+// const path = require('path')
 import  express  from "express"
 import { connectDb, getTodos, insertTodo } from './db/queries.js'
-// const path = require('path')
+import bodyParser from "body-parser"
 
 const app = express()
+app.use(bodyParser.json())
+app.use(express.static('static')) // middleware express function to serve static files in a directory
+
+connectDb()
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await getTodos()
+    res.json(todos)
+  } catch(err) {
+    console.log(err)
+  }
+})
+
+app.post('/addTodo', async (req, res) => {
+  await insertTodo(req.body.todo)
+  res.json(200)
+})
+
+app.post('/update/:id/:property', (req, res) => {
+
+})
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000")
+})
 
 // app.use((req, res, next) => {
 //   console.log(req.path)
 //   next()
 // })
-
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded  // app.use(express.json()) // for parsing application/json // https://expressjs.com/en/4x/api.html#req
-
-connectDb()
-
-app.get('/todos', async (req, res) => {
-  const todos = await getTodos()
-  res.json(todos) // *
-})
-
-app.post('/addTodo', async (req, res) => {
-  console.log(req)
-  // await insertTodo(req.body)
-  // res.send('200')
-  res.json('todo added')
-  // res.redirect('/')
-})
-
-app.use(express.static('static')) // middleware express function to serve static files in a directory
-
-
-app.listen(3000)
