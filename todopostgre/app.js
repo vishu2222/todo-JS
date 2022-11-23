d78c1e73449c5722526f14923d745e7377f74cc1
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-23 12:10:54 +0530

    added try catch to routes

--- todopostgre/app.js
+++ todopostgre/app.js
@@ -11,35 +11,51 @@ connectDb()
 app.get('/todos', async (req, res) => {
   try {
     const todos = await getTodos()
-    res.json(todos)
+    res.status(200).json(todos)
   } catch(err) {
     console.log(err)
   }
 })
 
 app.post('/addTodo', async (req, res) => {
-  await insertTodo(req.body.todo)
+  try {
+    await insertTodo(req.body.todo)
+  } catch(e) {
+    console.log(e)
+    res.json(`couldn't add`)
+  } 
   res.json(200)
 })
 
 app.post('/update/:id/:property', async (req, res) => {
-  const id = req.params.id.slice(1)
-  const property = req.params.property.slice(1)
-  const updatedVal = req.body.updatedVal
-  const dbRes = await updateTodo(id, property, updatedVal)
-  if (dbRes.rowCount === 1 ) { res.json(200) }
-  else { res.json('Error') }
+  try {
+    const id = req.params.id.slice(1)
+    const property = req.params.property.slice(1)
+    const updatedVal = req.body.updatedVal
+    const dbRes = await updateTodo(id, property, updatedVal)
+    if (dbRes.rowCount === 1 ) { res.json(200) }
+    else { res.json('Error') }
+    } catch(e) {
+      console.log(e)
+    }
 })
 
 app.delete('/delete/:id', async (req, res) => {
-  const id = req.params.id.slice(1)
-  const dbRes = await deleteTodo(Number(id))
-  res.json(`Recieved delete request for id ${id}`)
+  try {
+    const id = req.params.id.slice(1)
+    const dbRes = await deleteTodo(Number(id))
+    if (dbRes.rowCount === 1) {
+      res.json(`Recieved delete request for id ${id}`)
+    } else { res.json('Error') }
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 app.delete('/deleteDone', async (req, res) => {
   try {
     const dbRes = await deleteDone()
+    console.log(dbRes)
     if ( dbRes.rowCount >= 0 ) {
       res.json(200)
     }
@@ -51,14 +67,14 @@ app.delete('/deleteDone', async (req, res) => {
 app.get('/getCompleted', async (req, res) => {
   try {
     const dbRes = await getCompleted()
-    res.json(dbRes.rows)
+    res.status(200).json(dbRes.rows)
   } catch(e) { console.log(e) }
 })
 
 app.get('/getPending', async (req, res) => {
   try {
     const dbRes = await getPending()
-    res.json(dbRes.rows)
+    res.status(200).json(dbRes.rows)
   } catch(e) { console.log(e) }
 })
 
