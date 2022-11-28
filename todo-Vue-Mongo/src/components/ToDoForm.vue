ede7e4c754c74a6a4ae45ee112376b54fdb38b63
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-28 23:25:17 +0530

    removed helloWorld.vue and added ToDoForm.vue

---
+++ todo-Vue-Mongo/src/components/ToDoForm.vue
@@ -0,0 +1,29 @@
+<!-- template -->
+<template>
+    <form @submit.prevent = "submitNewTodo">
+        <label><strong>To-Do</strong></label>
+        <input type="text" required v-model="todoTxtInput">
+        <button>Add</button>
+    </form>
+</template>
+
+<!-- script -->
+<script>
+export default {
+    data () {
+        return {
+            todoTxtInput: ''
+        }
+    },
+    methods: {
+        submitNewTodo () {
+            this.$emit('todoAdded', this.todoTxtInput)
+        }
+    }
+}
+</script>
+
+<!-- style -->
+<style>
+
+</style>
