fad460709cabc050ee7e7f25768c6ec3e453b154
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-29 09:18:08 +0530

    todoContainerDiv, TodoItenComponent

--- todo-Vue-Mongo/src/App.vue
+++ todo-Vue-Mongo/src/App.vue
@@ -1,38 +1,50 @@
 <!-- template start-->
 <template>
-<ToDoForm @todoAdded="addTodo"></ToDoForm>
+  <ToDoForm @todoAdded="addTodo"></ToDoForm>
+
+  <div id="todoContainer">
+    <div v-for="todo in todosArr" :key="todo.id">
+      <TodoItem></TodoItem>
+    </div>
+  </div>
+
 </template>
 
 <!-- script start-->
 <script>
 import ToDoForm from './components/ToDoForm.vue'
+import TodoItem from './components/ToDoItem.vue'
 
 export default {
-  data () {
+  data() {
     return {
       id: 1,
-      todosArr: [{ "id": '1', "txt": "todo1", "date": null, "priority": null, "notes": null, "checkbox": false }]
+      todosArr: [
+        { "id": '1', "txt": "todo1", "date": null, "priority": null, "notes": null, "checkbox": false },
+        { "id": '2', "txt": "todo2", "date": null, "priority": null, "notes": null, "checkbox": true }
+      ]
     }
   },
 
-  components: { ToDoForm },
+  components: { ToDoForm, TodoItem },
 
   methods: {
-    addTodo (todoTxtInput) {
+    addTodo(todoTxtInput) {
       this.id += 1
-      this.todosArr.push({'id': this.id, 'txt': todoTxtInput})
-      console.log(this.todosArr)
+      this.todosArr.push({ 'id': this.id, 'txt': todoTxtInput })
     }
   },
 
-  mounted () {
-    
+  mounted() {
+
   }
-  
+
 }
 </script>
 
 <!-- style start-->
 <style>
-
+#todoContainer {
+  font-size: 150%;
+}
 </style>
