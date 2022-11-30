6c8cb15898267be6c47c46dcb976d1930b04f118
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-30 21:16:21 +0530

    formatted code

--- todo-Vue-Mongo/server/server.js
+++ todo-Vue-Mongo/server/server.js
@@ -1,21 +1,15 @@
-import express from "express"
+import express from 'express'
 import { getAll } from './db/queries.js'
-import cors from   'cors'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
-app.use(
-    cors({
-      origin: '*',
-      methods: ['GET', 'PUT', 'DELETE', 'POST']
-    })
-  )
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] })
+)
 
 app.get('/', async (req, res) => {
-  console.log('got request')
-  let result = await getAll()
-  console.log(result)
+  const result = await getAll()
   res.json(result)
 })
 
-app.listen(3000, () => { console.log("Listening on port: 3000") })
+app.listen(3000, () => { console.log('Listening on port: 3000') })
