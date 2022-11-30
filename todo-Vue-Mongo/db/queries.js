7e2790ed0a7c8bdb34ef4a37f168ef2e27107912
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-30 17:42:57 +0530

    added mongose client,schema model and getAll todos

---
+++ todo-Vue-Mongo/db/queries.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost:27017/todoDb')
+  .then(console.log('connected to DB on port 27017:'))
+  .catch(err => console.log('Couldnt connect to db:', err))
+
+const todoSchema = new mongoose.Schema( {
+  id: String,
+  txt: String,
+  date: String,
+  priority: String,
+  notes: String,
+  checkbox: Boolean
+})
+
+const todoModel =  mongoose.model('todos', todoSchema) // todos is the collection name 
+
+export function getAll () {
+  return todoModel.find()
+}
