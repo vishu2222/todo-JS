72d7b62935bbb5531c8b7b081f155de836c112d3
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-11-30 17:41:05 +0530

    added get request

--- todo-Vue-Mongo/server.js
+++ todo-Vue-Mongo/server.js
@@ -1,5 +1,21 @@
 import express from "express"
+import { getAll } from './db/queries.js'
+import cors from   'cors'
 
 const app = express()
+app.use(express.json())
+app.use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'PUT', 'DELETE', 'POST']
+    })
+  )
+
+app.get('/', async (req, res) => {
+  console.log('got request')
+  let result = await getAll()
+  console.log(result)
+  res.json(result)
+})
 
 app.listen(3000, () => { console.log("Listening on port: 3000") })
