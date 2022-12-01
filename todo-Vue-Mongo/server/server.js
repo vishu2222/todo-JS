79745d5acf06e443be2da61d33b33bb37d896215
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-01 15:19:21 +0530

    route to post todo

--- todo-Vue-Mongo/server/server.js
+++ todo-Vue-Mongo/server/server.js
@@ -1,15 +1,25 @@
 import express from 'express'
-import { getAll } from './db/queries.js'
+import { getAllTodos, insertTodo } from './db/queries.js'
 import cors from 'cors'
 
 const app = express()
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
 app.use(express.json())
-app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] })
-)
 
 app.get('/', async (req, res) => {
-  const result = await getAll()
-  res.json(result)
+  try {
+    const todos = await getAllTodos()
+    res.json(todos)
+  } catch (err) { res.send(500) }
 })
+
+app.post('/addTodo', async (req, res) => {
+  try {
+    await insertTodo(req.body)
+    return res.send('done')
+  } catch (err) { res.send(500) }
+})
+
+
 
 app.listen(3000, () => { console.log('Listening on port: 3000') })
