9747dca54b33d016d8c869fb0139a9c3f8d403b6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-02 23:40:02 +0530

    scoped style

--- todo-Vue-Mongo/client/src/App.vue
+++ todo-Vue-Mongo/client/src/App.vue
@@ -1,29 +1,34 @@
-<!-- template start-->
+<!-- template -->
 <template>
-  <ToDoForm @todoAdded="addTodo"></ToDoForm>                                <!-- addTodo is called with todoTxtInput emitted from todoform component-->
+  <ToDoForm @todoAdded="addTodo"/>                           <!-- addTodo is called with todoTxtInput emitted from todoform component-->
   <div id="todoContainer">                                                  <!-- The key allows Vue to accurately move each element to match the position of its corresponding object in the array -->
     <div v-for="todo in todosArr" :key="todo._id">
-      <TodoItem :item =todo @reRender="fetchData"></TodoItem>
+      <TodoItem :item =todo @reRender="fetchData"/>
     </div>
   </div>
+  <!-- <ToDoFooter></ToDoFooter> -->
 </template>
 
-
-
+<!-- script -->
 <script>
 import ToDoForm from './components/ToDoForm.vue'
 import TodoItem from './components/ToDoItem.vue'
+import ToDoFooter from './components/ToDoFooter.vue'
 import { fetchTodos, requestAddTodo } from './requests.js'
 
 export default {
+
+  // data
   data() {
     return {
       todosArr: []
     }
   },
 
-  components: { ToDoForm, TodoItem },
+  // components
+  components: { ToDoForm, TodoItem, ToDoFooter },
 
+  // methods
   methods: {
     async addTodo (todoTxt) {
      await requestAddTodo(todoTxt)
@@ -34,15 +39,15 @@ export default {
     }
   },
 
+  // lifeCycleHooks
   async created () {
     this.todosArr = await fetchTodos()
   }
 }
 </script>
 
-
-
-<style>
+<!-- style -->
+<style scoped> 
 #todoContainer {
   font-size: 150%;
 }
