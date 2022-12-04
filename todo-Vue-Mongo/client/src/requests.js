8ca358dbe1a24f65a413ec73c456db33d703252e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-04 15:35:49 +0530

    added reqs for delDone, delCompleted

--- todo-Vue-Mongo/client/src/requests.js
+++ todo-Vue-Mongo/client/src/requests.js
@@ -24,6 +24,14 @@ export async function requestDelete (id) {
   return result
 }
 
+export async function requestDeleteDone () {
+  const url = 'http://localhost:3000/deleteDone'
+  const result = await fetch(url, {
+    method: 'DELETE'
+  })
+  return result
+}
+
 export async function reqPropertyUpdate (id, property, val) {
   const url = 'http://localhost:3000/update/' + id
   const result = await fetch(url, {
@@ -32,4 +40,26 @@ export async function reqPropertyUpdate (id, property, val) {
     body: JSON.stringify({ todoProperty: property, todoPropertyVal: val })
   })
   return result
+}
+
+export async function requestDeleteCompleted () {
+  const url = 'http://localhost:3000/deleteDone'
+  const result = await fetch(url, {
+    method: 'DELETE'
+  })
+  return result
+}
+
+export async function requestCompleted () {
+  const url = 'http://localhost:3000/completed'
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
+}
+
+export async function requestPending () {
+  const url = 'http://localhost:3000/pending'
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
 }
