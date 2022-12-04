771342e0dd19dcef5b0a01a64b3cca248043a282
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-04 15:35:15 +0530

    added buttons deleteDone, show done, show pending

--- todo-Vue-Mongo/client/src/App.vue
+++ todo-Vue-Mongo/client/src/App.vue
@@ -1,12 +1,16 @@
 <!-- template -->
 <template>
   <ToDoForm @todoAdded="addTodo"/>                           <!-- addTodo is called with todoTxtInput emitted from todoform component-->
+  
   <div id="todoContainer">                                                  <!-- The key allows Vue to accurately move each element to match the position of its corresponding object in the array -->
     <div v-for="todo in todosArr" :key="todo._id">
       <TodoItem :item =todo @reRender="fetchData"/>
     </div>
   </div>
-  <!-- <ToDoFooter></ToDoFooter> -->
+
+  <button @click="toggleDisplay" class="display">{{display}}</button>
+  <button @click="deleteDone" class="deleteDone">Delete Done</button>
+
 </template>
 
 <!-- script -->
@@ -14,14 +18,15 @@
 import ToDoForm from './components/ToDoForm.vue'
 import TodoItem from './components/ToDoItem.vue'
 import ToDoFooter from './components/ToDoFooter.vue'
-import { fetchTodos, requestAddTodo } from './requests.js'
+import { fetchTodos, requestAddTodo, requestCompleted, requestPending, requestDeleteDone } from './requests.js'
 
 export default {
 
   // data
   data() {
     return {
-      todosArr: []
+      todosArr: [],
+      display: 'show done'
     }
   },
 
@@ -36,6 +41,26 @@ export default {
     },
     async fetchData () {
       this.todosArr = await fetchTodos()
+    },
+    async getPending() {
+      this.todosArr = await requestPending()
+    },
+    async deleteDone() {
+      await requestDeleteDone()
+      this.todosArr = await fetchTodos()
+    },
+    async toggleDisplay() {
+      if (this.display === 'show done') {
+        this.todosArr = await requestCompleted()
+        this.display = 'show pending'
+      } else if (this.display === 'show pending') {
+        this.todosArr = await requestPending()
+        this.display = 'show all'
+      } else {
+        this.todosArr = await fetchTodos()
+        this.display = 'show done'
+      }
+      
     }
   },
 
@@ -51,4 +76,16 @@ export default {
 #todoContainer {
   font-size: 150%;
 }
+
+.display {
+  display: inline-block;
+  margin-left: 500px;
+}
+
+.deleteDone {
+  display: inline-block;
+  margin-left: 730px;
+}
 </style>
+
+
