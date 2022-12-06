335ef561cc35a4f7435e93bbbefcfb9041978bba
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-06 21:46:51 +0530

    added eventListeners and mapped inputs to states

--- todo-React-redis/client/my-app/src/components/TodoItem.jsx
+++ todo-React-redis/client/my-app/src/components/TodoItem.jsx
@@ -1,14 +1,37 @@
-export default function TodoItem({ item }) {
+import { useState } from "react";
+import "./TodoItem.css";
+
+function TodoItem({ item }) {
+  // useState
+  const [txt, setTxt] = useState(item.txt);
+  const [checkbox, setCheckbox] = useState(item.checkbox);
+  const [notes, setNotes] = useState(item.notes);
+  const [date, setDate] = useState(item.date);
+  const [priority, setPriority] = useState(item.priority);
+  // return
   return (
     <div className="todoItem">
-      <input type="checkbox" value={item.checkbox} />
-      <input type="text" value={item.txt} />
+      <input
+        type="checkbox"
+        checked={checkbox}
+        onChange={(e) => setCheckbox(e.target.checked)}
+      />
+      <input type="text" value={txt} onChange={(e) => setTxt(e.target.value)} />
       <div className="properties">
-        <textarea cols="30" rows="10" value={item.notes} />
+        <textarea
+          cols="30"
+          rows="10"
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+        />
         <label>Due Date</label>
-        <input type="date" value={item.date} />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
         <label>Priority</label>
-        <select value={item.priority}>
+        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
           <option>None</option>
           <option>Low</option>
           <option>Medium</option>
@@ -19,3 +42,5 @@ export default function TodoItem({ item }) {
     </div>
   );
 }
+
+export default TodoItem;
