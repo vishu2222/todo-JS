ceac5ae312ddf8dccbf2481e43a7dd069863197b
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-06 21:44:23 +0530

    removed a todo from todos and added container div

--- todo-React-redis/client/my-app/src/App.jsx
+++ todo-React-redis/client/my-app/src/App.jsx
@@ -11,27 +11,19 @@ const todos = [
     priority: "Medium",
     notes: "notes1",
   },
-  {
-    id: 2,
-    checkbox: false,
-    txt: "todo1",
-    date: "2022-12-22",
-    priority: "Low",
-    notes: "notes2",
-  },
 ];
 
 export default function App() {
   return (
     <div>
       <TodoForm />
-      <div className="itemDiv">
-        {todos.map((item, index) => {
-          return <TodoItem key={index} item={item} />;
-        })}
+      <div id="todoContainer">
+        <div className="itemDiv">
+          {todos.map((item, index) => {
+            return <TodoItem key={index} item={item} />;
+          })}
+        </div>
       </div>
     </div>
   );
 }
-
-// <TodoItem item={todos[0]} />
