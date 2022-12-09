5d8b8a13bbda700952b3a3b8fc1875c59590f3ce
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-09 10:50:37 +0530

    changed todoId to nextId, setTods to setTodos

--- todo-React-redis/client/my-app/src/App.jsx
+++ todo-React-redis/client/my-app/src/App.jsx
@@ -1,31 +1,27 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm.jsx";
 import TodoItem from "./components/TodoItem.jsx";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function App() {
-  const [todos, setTods] = useState(
+  // state
+  const [todos, setTodos] = useState(
     JSON.parse(window.localStorage.getItem("todos"))
   );
-
   // methods
   function fetchTodos() {
-    setTods(JSON.parse(window.localStorage.getItem("todos")));
+    setTodos(JSON.parse(window.localStorage.getItem("todos")));
   }
 
-  useEffect(() => {
-    console.log("todos on updation", todos);
-  }, [todos]);
-
-  function addTodoTxt(txt) {
-    const todoId = todos.reduce((max, todo) => {
+  function addTodo(txt) {
+    const nextId = todos.reduce((max, todo) => {
       if (todo.id > max) {
         max = todo.id;
       }
       return max + 1;
     }, 0);
 
-    const newTodos = JSON.stringify([...todos, { id: todoId, txt: txt }]);
+    const newTodos = JSON.stringify([...todos, { id: nextId, txt: txt }]);
     window.localStorage.setItem("todos", newTodos);
     fetchTodos();
   }
@@ -36,15 +32,24 @@ export default function App() {
     fetchTodos();
   }
 
+  function updateTodo() {
+    console.log("");
+  }
+
   // component return
   return (
     <div>
-      <TodoForm todoTxt={addTodoTxt} />
+      <TodoForm addTodoProp={addTodo} />
       <div id="todoContainer">
         <div className="itemDiv">
-          {todos.map((item, index) => {
+          {todos.map((item) => {
             return (
-              <TodoItem key={item.id} todoItem={item} deleteTodo={deleteTodo} />
+              <TodoItem
+                key={item.id}
+                todoItem={item}
+                deleteTodo={deleteTodo}
+                updateTodo={updateTodo}
+              />
             );
           })}
         </div>
