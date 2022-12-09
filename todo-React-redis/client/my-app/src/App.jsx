ab17dfd22d881453ec14706da430d78f77b6cc04
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-09 14:59:55 +0530

    added updateTodo

--- todo-React-redis/client/my-app/src/App.jsx
+++ todo-React-redis/client/my-app/src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import TodoForm from "./components/TodoForm.jsx";
-import TodoItem from "./components/TodoItem.jsx";
-import { useState } from "react";
+import './App.css';
+import TodoForm from './components/TodoForm.jsx';
+import TodoItem from './components/TodoItem.jsx';
+// import TodoFooter from './components/TodoFooter.jsx';
+import { useState } from 'react';
 
 export default function App() {
   // state
-  const [todos, setTodos] = useState(
-    JSON.parse(window.localStorage.getItem("todos"))
-  );
+  const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem('todos')));
+
   // methods
   function fetchTodos() {
-    setTodos(JSON.parse(window.localStorage.getItem("todos")));
+    setTodos(JSON.parse(window.localStorage.getItem('todos')));
   }
 
   function addTodo(txt) {
@@ -21,20 +21,31 @@ export default function App() {
       return max + 1;
     }, 0);
 
-    const newTodos = JSON.stringify([...todos, { id: nextId, txt: txt }]);
-    window.localStorage.setItem("todos", newTodos);
+    const newTodos = JSON.stringify([...todos, { id: nextId, txt: txt, checkbox: false, date: '' }]);
+    window.localStorage.setItem('todos', newTodos);
     fetchTodos();
   }
 
   function deleteTodo(id) {
-    const newTods = todos.filter((todo) => todo.id !== id);
-    window.localStorage.setItem("todos", JSON.stringify(newTods));
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    window.localStorage.setItem('todos', JSON.stringify(newTodos));
     fetchTodos();
   }
 
-  function updateTodo() {
-    console.log("");
+  function updateTodo(id, property, value) {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo[`${property}`] = value;
+        return todo;
+      } else return todo;
+    });
+    window.localStorage.setItem('todos', JSON.stringify(newTodos));
+    fetchTodos();
   }
+
+  // function filterTodos(status) {
+  //   console.log('status', status);
+  // }
 
   // component return
   return (
@@ -43,17 +54,11 @@ export default function App() {
       <div id="todoContainer">
         <div className="itemDiv">
           {todos.map((item) => {
-            return (
-              <TodoItem
-                key={item.id}
-                todoItem={item}
-                deleteTodo={deleteTodo}
-                updateTodo={updateTodo}
-              />
-            );
+            return <TodoItem key={item.id} todoItem={item} deleteTodo={deleteTodo} updateTodo={updateTodo} />;
           })}
         </div>
       </div>
+      {/* <TodoFooter filterProp={filterTodos} /> */}
     </div>
   );
 }
