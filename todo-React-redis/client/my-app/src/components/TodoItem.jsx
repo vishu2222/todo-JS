9afe0fee85fbad591858a1e3e944e5fee2183194
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-09 15:03:32 +0530

    todos properties update&delHandlers,  borderColor

--- todo-React-redis/client/my-app/src/components/TodoItem.jsx
+++ todo-React-redis/client/my-app/src/components/TodoItem.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import "./TodoItem.css";
+import { useState } from 'react';
+import './TodoItem.css';
 
-export default function TodoItem({ todoItem, deleteTodo }) {
+export default function TodoItem({ todoItem, deleteTodo, updateTodo }) {
   const id = todoItem.id;
   // useState
   const [txt, setTxt] = useState(todoItem.txt);
@@ -13,32 +13,51 @@ export default function TodoItem({ todoItem, deleteTodo }) {
 
   // methods
   function toggleDisplay(e) {
-    if (e.target.tagName === "DIV") {
+    if (e.target.tagName === 'DIV') {
       setDisplayProperty(!displayPropertyDiv);
     }
   }
 
-  // function update() {}
+  function updateHandler(property, value) {
+    updateTodo(id, property, value);
+  }
 
-  function delTodo() {
+  function deleteHandler() {
     deleteTodo(id);
+  }
+
+  function setBorderColor() {
+    if (priority === 'Low') return 'groove green';
+    if (priority === 'Medium') return 'groove orange';
+    if (priority === 'High') return 'groove red';
+  }
+
+  function textDecor() {
+    if (checkbox === true) return 'line-through';
+    return 'none';
   }
 
   // component return
   return (
-    <div className="todoItem" onClick={toggleDisplay}>
+    <div className="todoItem" onClick={toggleDisplay} style={{ border: setBorderColor() }}>
       <div className="txtDiv">
         <input
           type="checkbox"
           checked={checkbox}
-          onChange={(e) => setCheckbox(e.target.checked)}
+          onChange={(e) => {
+            setCheckbox(e.target.checked);
+            updateHandler('checkbox', e.target.checked);
+          }}
         />
         <input
           className="todoTxt-TodoItem"
           type="text"
           value={txt}
-          onChange={(e) => setTxt(e.target.value)}
-          style={{ width: (txt.length + 10) * 8 + "px" }}
+          onChange={(e) => {
+            setTxt(e.target.value);
+            updateHandler('txt', e.target.value);
+          }}
+          style={{ width: (txt.length + 3) * 7.5 + 'px', textDecoration: textDecor() }}
         />
       </div>
 
@@ -49,27 +68,36 @@ export default function TodoItem({ todoItem, deleteTodo }) {
             cols="30"
             rows="10"
             value={notes}
-            onChange={(e) => setNotes(e.target.value)}
+            onChange={(e) => {
+              setNotes(e.target.value);
+              updateHandler('notes', e.target.value);
+            }}
           />
           <label className="dateLabel">Due Date</label>
           <input
             className="dateInput"
             type="date"
             value={date}
-            onChange={(e) => setDate(e.target.value)}
+            onChange={(e) => {
+              setDate(e.target.value);
+              updateHandler('date', e.target.value);
+            }}
           />
           <label className="priorityLabel">Priority</label>
           <select
             className="priority"
             value={priority}
-            onChange={(e) => setPriority(e.target.value)}
+            onChange={(e) => {
+              setPriority(e.target.value);
+              updateHandler('priority', e.target.value);
+            }}
           >
             <option>None</option>
             <option>Low</option>
             <option>Medium</option>
             <option>High</option>
           </select>
-          <button onClick={delTodo}>Delete</button>
+          <button onClick={deleteHandler}>Delete</button>
         </div>
       )}
     </div>
