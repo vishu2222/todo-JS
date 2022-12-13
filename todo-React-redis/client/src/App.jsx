c70c220a94af91bde6dde21e2dbb114c989829ea
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-13 18:21:57 +0530

    added postTodo and fetch todos

--- todo-React-redis/client/src/App.jsx
+++ todo-React-redis/client/src/App.jsx
@@ -1,34 +1,29 @@
-import './App.css';
-import TodoForm from './components/TodoForm.jsx';
-import TodoItem from './components/TodoItem.jsx';
-// import TodoFooter from './components/TodoFooter.jsx';
-import { useState } from 'react';
+import "./App.css";
+import TodoForm from "./components/TodoForm.jsx";
+import TodoItem from "./components/TodoItem.jsx";
+import { useState, useEffect } from "react";
+import { fetchTodos, addTodo_request } from "./requests.js";
 
 export default function App() {
   // state
-  const [todos, setTodos] = useState(JSON.parse(window.localStorage.getItem('todos')));
+  const [todos, setTodos] = useState([]);
 
   // methods
-  function fetchTodos() {
-    setTodos(JSON.parse(window.localStorage.getItem('todos')));
-  }
-
-  function addTodo(txt) {
-    const nextId = todos.reduce((max, todo) => {
-      if (todo.id > max) {
-        max = todo.id;
-      }
-      return max + 1;
-    }, 0);
-
-    const newTodos = JSON.stringify([...todos, { id: nextId, txt: txt, checkbox: false, date: '' }]);
-    window.localStorage.setItem('todos', newTodos);
-    fetchTodos();
+  async function addTodo(txt) {
+    const newTodo = {
+      txt: txt,
+      checkbox: false,
+      date: "",
+      notes: "",
+      priority: "",
+    };
+    await addTodo_request(newTodo);
+    setTodos(await fetchTodos());
   }
 
   function deleteTodo(id) {
     const newTodos = todos.filter((todo) => todo.id !== id);
-    window.localStorage.setItem('todos', JSON.stringify(newTodos));
+    window.localStorage.setItem("todos", JSON.stringify(newTodos));
     fetchTodos();
   }
 
@@ -39,26 +34,34 @@ export default function App() {
         return todo;
       } else return todo;
     });
-    window.localStorage.setItem('todos', JSON.stringify(newTodos));
+    window.localStorage.setItem("todos", JSON.stringify(newTodos));
     fetchTodos();
   }
 
-  // function filterTodos(status) {
-  //   console.log('status', status);
-  // }
+  // useEffect
+  useEffect(() => {
+    fetchTodos().then((data) => {
+      setTodos(data);
+    });
+  }, []);
 
-  // component return
   return (
     <div>
       <TodoForm addTodoProp={addTodo} />
       <div id="todoContainer">
         <div className="itemDiv">
           {todos.map((item) => {
-            return <TodoItem key={item.id} todoItem={item} deleteTodo={deleteTodo} updateTodo={updateTodo} />;
+            return (
+              <TodoItem
+                key={item.id}
+                todoItem={item}
+                deleteTodo={deleteTodo}
+                updateTodo={updateTodo}
+              />
+            );
           })}
         </div>
       </div>
-      {/* <TodoFooter filterProp={filterTodos} /> */}
     </div>
   );
 }
