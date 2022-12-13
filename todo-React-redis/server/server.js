af4d5f2528ad024e81826a7a86196d437a846d42
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-13 18:23:50 +0530

    added routes to getTodos and add a Todo

--- todo-React-redis/server/server.js
+++ todo-React-redis/server/server.js
@@ -1,11 +1,33 @@
 import express from "express";
-
+import { connectRedis, getTodos, insertTodo } from './db.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors(({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] })))
+app.use(express.json())
 
+await connectRedis()
 
+app.get('/', async (req, res) => {
+    try {
+        const todos = await getTodos()
+        res.json(todos)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
+app.post('/', async (req, res) => {
+    try {
+        await insertTodo(req.body)
+        res.sendStatus(200)
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500)
+    }
+})
 
 app.listen(3003, () => {
-    console.log('server is live on port 3000:')
+    console.log('Express server is live:')
 })
